766b5a747b4da5266b18ff06655b5fcc1d0af898
Marianne Røsvik <marianne.rosvik@olavstoppen.no>
2017-08-29 13:14:36 +0200

    test

---
+++ styleguide/README.md
@@ -0,0 +1,17 @@
+# designsystem-styleguide
+
+**UNDER CONSTRUCTION**
+
+Vi bruker Pattern Lab for å lage og vedlikeholde frontend-kode for komponenter. Du finner Pattern Lab repoet vårt [her](https://github.com/Altinn/DesignSystem). Du har nå kommet til "utstillingsvinduet" av komponentene og designsystemet. Det er satt opp ved hjelp av Brad Frost's [Style Guide Guide](https://github.com/bradfrost/style-guide-guide).
+
+En gulp-task eksporterer alle komponentene fra” PatternLab” til “Style Guide Guide”, og lager en bedre fremvisning av komponentene, uten å forholde seg til atomisk inndeling.
+
+Pattern Lab fungerer dermed som “arbeidsbenken” for frontendutviklere (hvor man jobber med atomisk inndeling), mens “Style Guide Guide” er “utstillingsvinduet”.
+
+## Setup
+1. Download or clone the files.
+2. In the command line, navigate to the root of the project and run the `jekyll serve` command. This will build the static site and watch for changes.
+3. Visit `http://127.0.0.1:4000/` in your browser to see the style guide.
+
+## More info
+Coming soon!
---
+++ styleguide/js/style-guide-custom.js
@@ -0,0 +1,196 @@
+/**
+ * Style Guide specific JavaScript CUSTOM
+ */
+
+
+ /*
+ * SWITCH BETWEEN PROJECTS
+ */
+ var that = this;
+ $('body').on('click', '[data-toggle="altinn-dropdown"] .a-dropdown-item', function() {
+ 	var $dropdownElement = $(this).closest('[data-toggle="altinn-dropdown"');
+ 	if ($(this).data('value')) {
+ 		$dropdownElement.find('.a-js-altinnDropdown-value').val($(this).data('value'));
+ 	}
+
+ 	$dropdownElement.find('.a-dropdown-toggle').html($(this).html());
+ });
+
+
+ /*
+ * VIEW CORRECT NAV BASED ON CHOSEN PROJECT
+ */
+
+ $(".primary-nav-altinnett").hide();
+ $(".primary-nav-brreg").hide();
+
+ $("[data-toggle='altinn-dropdown']").find(".dropdown-item").on("click", function() {
+  switch($(this).attr("data-value")) {
+    case "1":
+      $(".primary-nav-altinnett").hide();
+      $(".primary-nav-brreg").hide();
+      $(".primary-nav-altinn").show();
+      $('link[rel=stylesheet][href~="/styleguide/css/altinnett.css"]').remove();
+      $('link[rel=stylesheet][href~="/styleguide/css/brreg.css"]').remove();
+      break;
+    case "2":
+      $(".primary-nav-altinnett").show();
+      $(".primary-nav-altinn").hide();
+      $(".primary-nav-brreg").hide();
+      $("head link[rel='stylesheet']").last().after("<link rel='stylesheet' href='/styleguide/css/altinnett.css' type='text/css' media='screen'>");
+      $('link[rel=stylesheet][href~="/styleguide/css/brreg.css"]').remove();
+      break;
+    case "3":
+      $(".primary-nav-altinn").hide();
+      $(".primary-nav-altinnett").hide();
+      $(".primary-nav-brreg").show();
+      $("head link[rel='stylesheet']").last().after("<link rel='stylesheet' href='/styleguide/css/brreg.css' type='text/css' media='screen'>");
+      $('link[rel=stylesheet][href~="/styleguide/css/altinnett.css"]').remove();
+      break;
+  }
+});
+
+
+$(".fruit").onchange = function() {
+    localStorage.setItem('.fruit', document.getElementById(".fruit").value);
+}
+
+if (localStorage.getItem('.fruit')) {
+    $(".fruit").options[localStorage.getItem('.fruit')].selected = true;
+}
+
+ /*!
+ * TOGGLE navbar
+ 	+ hide the others
+ 	+ close when clicking outside of navbar
+ */
+ ;(function ($, undefined) {
+
+     $(window).resize(function() {
+         // This will fire each time the window is resized:
+         if($(window).width() < 768) {
+             // if larger or equal
+
+             $('body .a-st-overlay').animate({
+         			opacity: "0"
+         		}, 200, function(){
+         			$('body .a-st-overlay').remove();
+         		});
+
+             $(".a-st-expandable:visible").animate({
+         			left: "-300px"
+         		}, 200, function() {
+         		});
+
+             $(".a-st-toggleNavbar:visible").animate({
+         			left: "0px"
+
+         		}, 200, function() {
+         		});
+             $(".a-st-toggleNavbar").removeClass('open');
+         } else {
+           $(".a-st-expandable").animate({
+     				left: "0"
+     			}, 200, function() {
+     				// Animation complete.
+     			});
+           $(".a-st-toggleNavbar").animate({
+     				left: "280px"
+     			}, 200, function() {
+     				// Animation complete.
+     			});
+           $(".a-st-toggleNavbar").addClass('open');
+         }
+     }).resize(); // This will simulate a resize to trigger the initial run.
+
+ 	function openExpandable($element){
+ 		var expandable = "";
+
+ 		if($element.hasClass("a-st-toggleNavbar")){
+ 			expandable = ".a-st-navbar";
+ 		}
+
+ 		$element.addClass('open');
+
+ 		$('<div class="a-st-overlay"></div>').appendTo('body');
+
+ 		$('body .a-st-overlay').animate({
+ 			opacity: "0.6"
+ 		}, 200, function(){
+
+ 		});
+
+ 		if(expandable !== ""){
+ 			$(expandable+".a-st-expandable").animate({
+ 				left: "0"
+ 			}, 200, function() {
+ 				// Animation complete.
+ 			});
+       $(".a-st-toggleNavbar").animate({
+ 				left: "280px"
+ 			}, 200, function() {
+ 				// Animation complete.
+ 			});
+ 		}
+ 	}
+
+ 	function closeExpandable($element){
+ 		$('body .a-st-overlay').animate({
+ 			opacity: "0"
+ 		}, 200, function(){
+ 			$('body .a-st-overlay').remove();
+ 		});
+
+ 	  $(".a-st-expandable:visible").animate({
+ 			left: "-300px"
+
+ 		}, 200, function() {
+
+ 		});
+
+     $(".a-st-toggleNavbar:visible").animate({
+ 			left: "0px"
+
+ 		}, 200, function() {
+
+ 		});
+
+ 		$element.removeClass('open');
+ 	}
+
+ 	function addClickOutsideExpandableEvent(){
+ 		$(document).mouseup(function (e){
+ 			if(!$(e.target).hasClass("a-st-toggleNavbar-expandable open")){
+ 				var $openExpandable = $(".a-st-expandable:visible");
+ 				var testClass = $(e.target).hasClass('.a-st-overlay');
+
+ 				if(testClass == true){
+ 					closeExpandable($("header .a-st-toggleNavbar-expandable.open"));
+           $(".a-st-toggleNavbar").removeClass('open');
+ 				}
+ 			}
+
+ 			if($(e.target).hasClass('a-st-overlay') || $(e.target).hasClass('icon-cancel')){
+ 				closeExpandable($("header .a-st-toggleNavbar-expandable.open"));
+         $(".a-st-toggleNavbar").removeClass('open');
+ 			}
+ 		});
+ 	}
+
+ 	$(function () {
+ 		addClickOutsideExpandableEvent();
+ 		$('.a-st-toggleNavbar-expandable').click(function () {
+ 			var self = $(this);
+
+ 			if (self.hasClass('open')) {
+ 				closeExpandable(self);
+ 			} else {
+ 				closeExpandable($("header .a-st-toggleNavbar-expandable.open"));
+ 				openExpandable(self);
+ 			}
+ 			return false;
+ 		})
+
+ 	});
+
+ }(jQuery));
