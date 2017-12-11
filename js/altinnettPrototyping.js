d1617faef1812be7677e2b4baedf32f1f33ed29c
bjorgdagny <bdk@brreg.no>
2017-12-11 12:10:16 +0100

    Brreg/bdk/change switch to switch between css (#4)
    * * Changed the head to include the dist-css and style.css
    * Changed custom.js to use the right css based on project
    
    * * Moved the brreg logo black background
    
    * * Added the changed js from pattern lab
    
    * * Changed patterns after copying from Designsystem. Reason is changed file name on css

--- js/altinnettPrototyping.js
+++ js/altinnettPrototyping.js
@@ -1,19 +1,3 @@
-/* globals $ */
-var fixPatternLinks = function() {
-  if (window.location.pathname.indexOf('DesignSystem') === 1) {
-    $('a').each(function() {
-      if (typeof $(this).attr('href') !== typeof undefined && $(this).attr('href') !== false && $(this).attr('href').indexOf('DesignSystem') === -1) {
-        $(this).attr('href', $(this).attr('href').replace('/patterns/', '/DesignSystem/public/patterns/'));
-      }
-    });
-    $('*[onclick]').each(function() {
-      if ($(this).attr('onclick').indexOf('location.href=\'/patterns/') > -1) {
-        $(this).attr('onclick', $(this).attr('onclick').replace('location.href=\'/patterns/', 'location.href=\'/DesignSystem/public/patterns/'));
-      }
-    });
-  }
-};
-
 var setupTruncateLines = function() {
   setTimeout(function() {
     // Max two lines for all screen sizes
@@ -646,15 +630,10 @@ AltinnModal = {
 };
 
 /* globals
-  fixPatternLinks,
-  AltinnModal,
+  AltinnModal
   setupTruncateLines
 */
 window.altinnettInit = function() {
-  // Only for prototyping
-  fixPatternLinks();
-
-  // Should also be included in production (dist)
   AltinnModal.init();
   setupTruncateLines();
 };
